c49eb928a11353d23405dd46557227e80a226548
José Levy Rodrigues da Silva <131412804+levyrodrigues23@users.noreply.github.com>
2026-02-14 22:48:33 -0300

    Time: 58 ms (40.27%) | Memory: 147.4 MB (94.27%) - LeetSync

---
+++ 1-two-sum/two-sum.dart
@@ -0,0 +1,16 @@
+class Solution {
+  List<int>? twoSum(List<int> nums, int target) {
+    for(var i = 0; i < nums.length; i++){
+        
+        for(var j = i + 1; j < nums.length; j++){
+            if(nums[i] + nums[j] == target){
+                return [i, j];
+            } 
+            
+            
+        }
+      
+    }
+
+  }
+}
