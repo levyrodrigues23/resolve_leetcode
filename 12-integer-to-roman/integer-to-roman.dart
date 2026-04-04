6d64b7a2e27376f4c2703a8a9cbfb07a0660ee8a
José Levy Rodrigues da Silva <131412804+levyrodrigues23@users.noreply.github.com>
2026-04-04 01:05:56 -0300

    Time: 65 ms (6.35%) | Memory: 150 MB (66.67%) - LeetSync

---
+++ 12-integer-to-roman/integer-to-roman.dart
@@ -0,0 +1,38 @@
+
+
+class Solution {
+  String intToRoman(int num) {
+    final Map<int, String> romanMap = {
+           1000: 'M',
+    900: 'CM',
+    500: 'D',
+    400: 'CD',
+    100: 'C',
+    90: 'XC',
+    50: 'L',
+    40: 'XL',
+    10: 'X',
+    9: 'IX',
+    5: 'V',
+    4: 'IV',
+    1: 'I',
+    };
+
+    String result = '';
+
+  for(MapEntry<int, String> entry in romanMap.entries){
+    var key = entry.key;
+    var value = entry.value;
+    while(num >= key){
+        result += value;
+        num = num - key;
+    }
+  
+    
+
+  }
+
+  return result;
+  }
+
+}
